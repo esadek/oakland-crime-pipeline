e5fdb2222f6f7aa8ae4c1a2ff41ada5beab89f37
Emil Sadek <esadek@hotmail.com>
2020-07-25 20:23:52 -0700

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 from sodapy import Socrata
 from prefect import task, Flow
 from prefect.tasks.database.sqlite import SQLiteScript
@@ -6,27 +7,28 @@ from prefect.tasks.database.sqlite import SQLiteScript
 
 @task
 def extract_crime_data():
-    """Extract JSON crime data from API."""
     client = Socrata('data.oaklandnet.com', None)
-    results = client.get_all('ym6k-rx7a')
-    return results
+    return client.get_all('ym6k-rx7a')
 
 
 @task
 def transform_crime_data(data):
-    """Transform crime data."""
-    df = pd.DataFrame.from_records(data) # Convert data to DataFrame
-    df = df.rename(columns={'location_1': 'location'}) # Rename location column
+    # Convert data to DataFrame
+    df = pd.DataFrame.from_records(data)
+    # Rename location column
+    df = df.rename(columns={'location_1': 'location'})
+    # Drop unnecessary columns
     to_drop = ['city', 'state', ':@computed_region_w23w_jfhw']
-    df.drop(to_drop, axis=1, inplace=True) # Drop unnecessary columns
-    df = df[df['datetime'].str.startswith('2020')] # Drop rows not from 2020
-    df['datetime'] = pd.to_datetime(df['datetime']) # Convert datetime
+    df.drop(to_drop, axis=1, inplace=True)
+    # Drop rows not from last 90 days
+    today = str(datetime.date.today())
+    ninety_days_ago = str(datetime.date.today() - timedelta(days=90))
+    df = df[(df['datetime'] >= ninety_days_ago) & (df['datetime'] <= today)]
     return df
 
 
 @task
 def load_crime_data(data):
-    """Load crime data into SQLite database."""
     pass
 
 with Flow('ETL') as flow:
