fafa8d45cdacfd8b70dc0e1cff54a2b929c7735e
Emil Sadek <esadek@hotmail.com>
2020-08-26 20:21:51 -0700

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,42 +1,36 @@
 import pandas as pd
 from datetime import date, timedelta
+from sqlalchemy import create_engine
 from sodapy import Socrata
-from prefect import task, Flow
-from prefect.tasks.database.sqlite import SQLiteScript
 
-SOURCE_DOMAIN = 'data.oaklandnet.com'
-DATASET_IDENTIFIER = 'ym6k-rx7a'
+def constrict_days(df, col, days):
+    '''Remove entries not from last specified number of days'''
+    today = str(date.today())
+    x_days_ago = str(date.today() - timedelta(days=days))
+    return df[(df[col] >= x_days_ago) & (df[col] <= today)]
 
-@task
 def extract():
     '''Retrieve crime data from Socrata API.'''
-    client = Socrata(SOURCE_DOMAIN, None)
-    results = client.get_all(DATASET_IDENTIFIER)
+    client = Socrata('data.oaklandnet.com', None)
+    results = client.get_all('ym6k-rx7a')
     return pd.DataFrame.from_records(results)
 
-@task
 def transform(data):
     '''Transform crime data.'''
-
-    # Drop unnecessary columns
     to_drop = ['city', 'state', 'location_1', ':@computed_region_w23w_jfhw']
     df = data.drop(to_drop, axis=1)
-
-    # Drop rows not from last 90 days
-    today = str(date.today())
-    ninety_days_ago = str(date.today() - timedelta(days=90))
-    df = df[(df['datetime'] >= ninety_days_ago) & (df['datetime'] <= today)]
-
+    df = constrict_days(df, 'datetime', 90)
+    df['datetime'] = pd.to_datetime(df['datetime'])
     return df
 
-@task
 def load(data):
     '''Load crime data into SQLite database.'''
-    pass
+    engine = create_engine('sqlite:///database.db')
+    sqlite_connection = engine.connect()
+    data.to_sql('crimes', sqlite_connection)
+    sqlite_connection.close()
 
-with Flow('ETL') as flow:
-    e = extract()
-    t = transform(e)
-    l = load(t)
-
-flow.run()
+# ETL pipeline
+(extract().pipe(transform)
+          .pipe(load)
+)
