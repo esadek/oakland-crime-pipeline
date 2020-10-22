a9338ce96b7248db64f3386a0f3b0b719cd9ea6e
Emil Sadek <esadek@hotmail.com>
2020-10-22 09:14:34 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 from flow import *
 
-if __name__ == '__main__':
-
-    # Execute ETL flow
-    flow.run(
-        source_domain = 'data.oaklandnet.com',
-        dataset_id = 'ym6k-rx7a',
-        to_drop = ['city', 'state', 'location_1', ':@computed_region_w23w_jfhw'],
-        column = 'datetime',
-        days = 90,
-        database = 'sqlite:///database.db',
-        table = 'crimes'
-    )
+# Execute ETL flow
+flow.run(
+    source_domain = 'data.oaklandnet.com',
+    dataset_id = 'ym6k-rx7a',
+    to_drop = ['city', 'state', 'location_1', ':@computed_region_w23w_jfhw'],
+    column = 'datetime',
+    days = 90,
+    database = 'sqlite:///database.db',
+    table = 'crimes'
+)
